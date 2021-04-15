313f03398654c39159ec4c16903c29a35d5886ba
Abdifatah Abdulkadir <zafex188@hotmail.com>
2021-04-15 18:54:05 +0200

    Added: readme and changed some style on css

--- src/route.js
+++ src/route.js
@@ -2,21 +2,6 @@ const express = require("express");
 const router = express.Router();
 const data_base = require("./dataBase.js"); // requiring database file and accessing the mysql queries
 
-
-router.get("/getAllCalendar/", async(req,res) =>{
-    try {
-        let dbResult = await data_base.getAllCalendar();
-        res.json(dbResult); // response from database will be returned as json
-        console.log("inside the route");
-        console.log(dbResult); //print the result to the console
-    } catch (error) {
-        console.log("JsonError" + error);
-        res.send("500");
-    }
-});
-
-
-
 router.get("/news", async(req, res) => {
     try {
         let dbResult = await data_base.module.news();
