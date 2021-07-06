a5b8e03411b8d9d232d3b35adb3fa81ff3ae4c31
abdifatahAbdulkadir <44999272+abdifatahAbdulkadir@users.noreply.github.com>
2021-07-06 20:40:57 +0200

    Update dataBase.js

--- src/dataBase.js
+++ src/dataBase.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 // mysql.createPool -> creates a connection with a connectionLimit of '10'
 let connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "root", //write your database password
+    user: "*",
+    password: "*", //write your database password
     database: "calendar", // your database name
     port: 3306,
     connectionLimit: 10,
